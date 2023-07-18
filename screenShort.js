fd711726b29b58c3284734aff8c8490c6c0cac30
bisht-xp <bishtkamal2907@gmail.com>
2023-07-18 20:33:17 +0530

    change: change the viewport

--- screenShort.js
+++ screenShort.js
@@ -5,6 +5,7 @@ async function takeScreenshot() {
   const page = await browser.newPage();
   await page.goto('https://real-state-alpha.vercel.app/');
   await page.screenshot({ path: 'public/screenshot.png' });
+  await page.setViewport({width: 1400, height: 1024});
   await browser.close();
 }
 

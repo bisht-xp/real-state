f0e5602327c7b10a5d50f57154cfcced568878c1
bisht-xp <bishtkamal2907@gmail.com>
2023-07-18 20:19:06 +0530

    fix: fixed bugs

--- screenShort.js
+++ screenShort.js
@@ -1,10 +1,11 @@
-import puppeteer from "puppeteer";
+const puppeteer = require('puppeteer');
 
-(async () => {
+async function takeScreenshot() {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto("https://real-state-alpha.vercel.app/");
-  await page.setViewport({width: 1400, height: 1024});
-  await page.screenshot({ path: "public/screenshot.png" });
+  await page.goto('https://real-state-alpha.vercel.app/');
+  await page.screenshot({ path: 'public/screenshot.png' });
   await browser.close();
-})();
+}
+
+takeScreenshot();

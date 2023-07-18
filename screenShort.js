527cddb19d0cd26a257645028163e1f88c0deead
bisht-xp <bishtkamal2907@gmail.com>
2023-07-18 20:03:21 +0530

    Change: changes the view port and screenshort

--- screenShort.js
+++ screenShort.js
@@ -4,7 +4,7 @@ import puppeteer from "puppeteer";
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto("https://real-state-alpha.vercel.app/");
-  await page.setViewport({width: 1200, height: 1024});
+  await page.setViewport({width: 1400, height: 1024});
   await page.screenshot({ path: "public/screenshot.png" });
   await browser.close();
 })();
